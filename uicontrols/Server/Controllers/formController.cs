c86942566f79dbfb783fb6698e4b08e792b9ae00
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2023-12-23 20:35:21 +0300

    Proje dosyası ekle.

---
+++ uicontrols/Server/Controllers/formController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using uicontrols.Shared;
+
+namespace uicontrols.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class formController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Save")]
+        public void Save(formModel fm)
+        {
+            StreamWriter sw = new StreamWriter("log.txt", true);
+            sw.WriteLine(fm.email);
+            sw.WriteLine(fm.password);
+            sw.WriteLine(fm.rememberchk);
+            sw.WriteLine(fm.select);
+            sw.WriteLine("----------------------------------");
+            sw.Close();
+        }
+    }   
+
+}
---
+++ uicontrols/Shared/formModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uicontrols.Shared
+{
+    public class formModel
+    {
+        public String email { get; set; }
+        public String password { get; set; }
+        public bool rememberchk { get; set; }
+        public String select {  get; set; }
+    }
+}
